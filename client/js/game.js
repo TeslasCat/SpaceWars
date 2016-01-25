3d0cb8a6ca770b92a3ce0b8fad97ce23239a843d
0x6C77 <0x6C77@gmail.com>
2016-01-25 20:30:20 +0000

    Refactored game code into game.js and added login+animation

--- client/js/game.js
+++ client/js/game.js
@@ -5,11 +5,91 @@ var game = {
     scale: 1,
     position: {x: 0, y: 0},
     panningSpeed: 500,
+    introLength: 750,
+
+    start: function() {
+        registerMouseListeners();
+        registerCommands();
+        $('body').addClass('playing');
+
+        this.gameStart = (new Date()).getTime();
+        this.draw(this.gameStart);
+    },
+    draw: function(lastDraw) {
+        var self = this,
+            now = (new Date()).getTime(),
+            gameDuration = now - this.gameStart,
+            duration = (now - lastDraw) / 1000,
+            lastDraw = now;
+        game.context.clearRect(0, 0, game.canvas.width, game.canvas.height);
+        game.context.setLineDash([]);
+
+        // Update game view
+        game.update(duration);
+
+        this.drawGrid(gameDuration);
+
+        if (gameDuration > this.introLength * 1.1) {
+            // Are we tracking something?
+            if (game.tracking) {
+                game.setPosition(game.tracking.getPlot());
+            }
+
+            for(i = game.planets.length - 1; i >= 0; i--) {
+                game.planets[i].draw();
+            }
+
+            for(i = game.ships.length - 1; i >= 0; i--) {
+                game.ships[i].move(duration);
+                game.ships[i].draw();
+            }
+        }
+
+        requestAnimFrame(function() {
+            self.draw(lastDraw);
+        });
+    },
+    drawGrid: function(duration) {
+        if (duration > this.introLength) {
+            lines = 1;
+        } else {
+            lines = duration / this.introLength;
+        }
+
+        var bgSize = 10 * game.scale;
+        var center = helper.getScreenPosition({x: 0, y: 0});
+
+        // Draw center lines
+        game.context.fillStyle = 'rgba(125, 0, 0, 0.3)';
+        if (center.x >= 0 && center.x <= gameCanvas.width) {
+            game.context.fillRect(center.x, 0, 1, game.canvas.height * lines);
+        }
+        if (center.y >= 0 && center.y <= gameCanvas.height) {
+            game.context.fillRect(0, center.y, game.canvas.width * lines, 1);
+        }
+
+        // Draw vertical lines
+        game.context.fillStyle = 'rgba(100, 100, 100, 0.1)';
+
+        var offset = Math.floor(center.x % bgSize),
+            i = Math.ceil(game.canvas.width * lines / bgSize) + 1;
+        while (i--) {
+            game.context.fillRect(offset + (i * bgSize), 0, 1, game.canvas.height * lines);
+        }
+
+        // Draw horizontal lines
+        offset = Math.floor(center.y % bgSize);
+        i = Math.ceil(game.canvas.height / bgSize) + 1;
+        while (i--) {
+            game.context.fillRect(0, offset + (i * bgSize), game.canvas.width * lines, 1);
+        }
+    },
+
+
     getPosition: function() {
         return c(this.position);
     },
     setPosition: function(position) {
-        console.log(position);
         position.x = -position.x;
         position.y = -position.y;
         this.targetPosition = {};
--- client/js/main.js
+++ client/js/main.js
@@ -1,11 +1,12 @@
-$(function() {
-    window.requestAnimFrame = (function(callback) {
-        return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame ||
-        function(callback) {
-          window.setTimeout(callback, 1000 / 60);
-        };
-    })();
+window.requestAnimFrame = (function(callback) {
+    return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame ||
+    function(callback) {
+      window.setTimeout(callback, 1000 / 60);
+    };
+})();
 
+
+$(function() {
     game.canvas = $('#gameCanvas').get(0);
     game.context = game.canvas.getContext('2d');
 
@@ -19,9 +20,6 @@ $(function() {
         game.canvas.width = $('#game').width();
         game.canvas.height = $('#game').height();
     });
-
-    registerMouseListeners();
-    registerCommands();
 
     game.the_player = null;
     game.players = [];
@@ -49,94 +47,22 @@ $(function() {
     tmpPlanet = new Planet('Hello', { x: 100, y: 100}, 5);
     game.planets.push(tmpPlanet);
 
-    // for (i = 0; i < 100; i++) {
 
-    //     name = planetNames[helper.rand(0, planetNames.length)] + '-' + i;
-    //     tmpLoc = {
-    //         x: helper.rand(0, game.canvas.width * 2) - game.canvas.width,
-    //         y: helper.rand(0, game.canvas.height * 2) - game.canvas.height
-    //     };
-    //     tmpRadius = helper.rand(5000, 15000);
-    //     tmpPlanet = new Planet(name, tmpLoc, tmpRadius);
-
-    //     mCount = helper.rand(0, 5);
-    //     for (j = 0; j < mCount; j++) {
-    //         /**/ tmpAltitude = Math.floor(Math.random() * ((tmpRadius/1000)*2)) + (tmpRadius/1000)*1.3;
-    //         /**/ tmpSpeed = Math.floor(Math.random() * 4) + 0.3;
-    //         /**/ tmpMoon = new Moon(name + '.' + j, tmpPlanet, 250, tmpAltitude, tmpSpeed);
-    //         tmpMoon.angle = Math.floor(Math.random() * 360);
-    //         tmpPlanet.moons.push(tmpMoon);
-    //     }
-
-    //     game.planets.push(tmpPlanet);
-    // }
+    $('#login .button').on('click', function(e) {
+        e.preventDefault();
+        $('#login').addClass('hidden');
+        setTimeout(function() {
+            $('#login').remove();
+            game.start();
+        }, 1000);
+    });
 
 
-    function drawBG() {
-        var bgSize = 10 * game.scale;
-        var center = helper.getScreenPosition({x: 0, y: 0});
-
-        // Draw center lines
-        game.context.fillStyle = 'rgba(125, 0, 0, 0.3)';
-        if (center.x >= 0 && center.x <= gameCanvas.width) {
-            game.context.fillRect(center.x, 0, 1, game.canvas.height);
-        }
-        if (center.y >= 0 && center.y <= gameCanvas.height) {
-            game.context.fillRect(0, center.y, game.canvas.width, 1);
-        }
-
-        // Draw vertical lines
-        game.context.fillStyle = 'rgba(100, 100, 100, 0.1)';
-
-        var offset = Math.floor(center.x % bgSize),
-            i = Math.ceil(game.canvas.width / bgSize) + 1;
-        while (i--) {
-            game.context.fillRect(offset + (i * bgSize), 0, 1, game.canvas.height);
-        }
-
-        // Draw horizontal lines
-        offset = Math.floor(center.y % bgSize);
-        i = Math.ceil(game.canvas.height / bgSize) + 1;
-        while (i--) {
-            game.context.fillRect(0, offset + (i * bgSize), game.canvas.width, 1);
-        }
-    }
-
-    function drawGame(lastDraw) {
-        var duration = ((new Date()).getTime() - lastDraw) / 1000,
-            lastDraw = (new Date()).getTime();
-        game.context.clearRect(0, 0, game.canvas.width, game.canvas.height);
-        game.context.setLineDash([]);
-
-        // Update game view
-        game.update(duration);
-
-        drawBG();
-
-        // Are we tracking something?
-        if (game.tracking) {
-            game.setPosition(game.tracking.getPlot());
-        }
-
-        for(i = game.planets.length - 1; i >= 0; i--) {
-            game.planets[i].draw();
-        }
-
-        for(i = game.ships.length - 1; i >= 0; i--) {
-            game.ships[i].move(duration);
-            game.ships[i].draw();
-        }
-
-        requestAnimFrame(function() {
-            drawGame(lastDraw);
-        });
-    }
-
-    lastDraw = (new Date()).getTime();
-    drawGame(lastDraw);
+    // setTimeout(function() {
+    //     game.start();
+    // }, 1000);
 });
 
-var epoch = Date.parse("January 30, 2016 14:20");
 function updateTimer() {
     $('.time').text(helper.getTime());
 
