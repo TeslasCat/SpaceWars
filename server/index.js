255c5be194fa5d8123a682425e9fb5c429d6c1b6
0x6C77 <0x6C77@gmail.com>
2016-02-10 21:47:05 +0000

    Returned Redis connect port to correct value

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ var BISON = require("./bison");
 var Ship = require("./ship");
 
 var redis = require("redis");
-var db = redis.createClient({host: 'www.hackthis.co.uk', port: 6370});
+var db = redis.createClient({port: 6379});
 
 /**
  * Message protocols
